25df9af5cded4b5cb03ae66842da8d22351cd5df
Developer-Ali <Ali--B@hotmail.de>
2022-06-06 19:25:18 +0200

    blocks can land via landing grid

--- BlockSpawner.gd
+++ BlockSpawner.gd
@@ -8,14 +8,13 @@ onready var spawn_position = $SpawnPosition
 var active_block :GeneralBlock
 
 var landing_grid = []
-var landing_grid_width = 10
-var landing_grid_height = 16
+var landing_grid_width = 10 * 8
+var landing_grid_height = 16 * 8
 
 
 
 func _ready():
 	init_landing_grid()
-	EventBus.connect("block_landed", self, "active_block_landed")
 	spawn_block()
 	tick.start()
 	
@@ -23,7 +22,22 @@ func init_landing_grid():
 	for i in landing_grid_width:
 		landing_grid.append([])
 		for j in landing_grid_height:
-			landing_grid[i].append(0) # Set a starter value for each position
+			landing_grid[i].append(null)
+	
+
+	
+func active_block_landed():
+	add_landed_block_to_grid()
+	active_block = null
+	spawn_block()
+	
+func add_landed_block_to_grid():
+	for square in active_block.get_children():
+		
+		var rounded_x = round(square.global_position.x) 
+		var rounded_y = round(square.global_position.y)
+		
+		landing_grid[rounded_x][rounded_y] = square.position
 	
 func spawn_block():
 	randomize()
@@ -32,28 +46,46 @@ func spawn_block():
 	block.position = spawn_position.position
 	active_block = block
 	add_child(block)
-	
-func active_block_landed():
-	active_block = null
-	spawn_block()
 
 func _on_Tick_timeout():
-	active_block.move_one_step()
+	active_block.move_down()
+	if(!check_valid_position()):
+		active_block.move_up()
+		active_block_landed()
 
 func _process(delta):
 	
 	if Input.is_action_just_pressed("ui_right"):
 		active_block.move_right()
+		if(!check_valid_position()):
+			active_block.move_left()
 		
 	if Input.is_action_just_pressed("ui_left"):
 		active_block.move_left()
+		if(!check_valid_position()):
+			active_block.move_right()
 		
 	if Input.is_action_pressed("ui_down"):
-		active_block.move_one_step()
+		_on_Tick_timeout()
 	
 	if Input.is_action_just_pressed("ui_up"):
 		active_block.rotate_block()
+		if(!check_valid_position()):
+			active_block.rotate_block_back()
 
 
+func check_valid_position() -> bool:
+	for square in active_block.get_children():
+		
+		var rounded_x = round(square.global_position.x) 
+		var rounded_y = round(square.global_position.y)
+		
+		if (rounded_x < 4) || (rounded_x > 76) || rounded_y > 124:
+			return false
+			
+		if (landing_grid[rounded_x][rounded_y] != null):
+			return false
+
+	return true
 
 
--- GeneralBlock.gd
+++ GeneralBlock.gd
@@ -3,37 +3,20 @@ class_name GeneralBlock
 
 var step :float = 8
 
-func move_one_step():
+func move_down():
 	position += Vector2(0,step)
-	if (!check_valid_position()):
-		position -= Vector2(0,step)
-		EventBus.emit_signal("block_landed")
+
+func move_up():
+	position -= Vector2(0,step)
 
 func move_right():
 	position += Vector2(step,0)
-	if (!check_valid_position()):
-		position -= Vector2(step,0)
 	
 func move_left():
 	position -= Vector2(step,0)
-	if (!check_valid_position()):
-		position += Vector2(step,0)
 	
 func rotate_block():
 	rotation_degrees += 90
-	if (!check_valid_position()):
-		rotation_degrees -= 90
 
-
-func check_valid_position() -> bool:
-	for square in get_children():
-		
-		var rounded_x = round(square.global_position.x) 
-		var rounded_y = round(square.global_position.y)
-		
-		print(rounded_x)
-		print(rounded_y)
-		if (rounded_x < 4) || (rounded_x > 76) || rounded_y > 124:
-			return false
-
-	return true
+func rotate_block_back():
+	rotation_degrees -= 90
